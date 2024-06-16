344792f7c305b4c61986310439ea9038cedcdc7a
misnina <misnina.k@gmail.com>
2024-06-16 08:43:06 -0500

    added dialogue queueing system

--- scripts/conversation_controller.gd
+++ scripts/conversation_controller.gd
@@ -4,6 +4,18 @@ extends Node2D
 @onready var character_speaking = $"Character Label/character_speaking"
 @onready var dialogue_container = $Text/dialogue
 
+var ready_dialogue = []
+signal dialogue_ended
+
+func read_from_queue():
+	for dialogue in ready_dialogue:
+		await display_dialogue(dialogue[0], dialogue[1], dialogue[2], dialogue[3], dialogue[4])
+	ready_dialogue.clear()
+
+func queue_dialogue(character, dialogue, expression, text_speed, speak_speed):
+	print('dialogue queued')
+	ready_dialogue.append([character, dialogue, expression, text_speed, speak_speed])
+
 func change_label(character_name):
 	character_speaking.text = "[center]%s[/center]" % character_name
 
@@ -15,9 +27,13 @@ func display_dialogue(character, dialogue, expression, text_speed, speak_speed):
 		dialogue_container.text = displayed_text + char
 		await get_tree().create_timer(speak_speed).timeout
 		displayed_text = displayed_text + char
+	dialogue_ended.emit()
 
 func translate_time_from_dialogue(dialogue, text_speed):
-	return dialogue.length() / text_speed
+	if (dialogue.length() / text_speed) <= 0 :
+		return 0.8
+	else:
+		return dialogue.length() / text_speed
 
 func seperated_text(dialogue):
 	var text_array = []
--- scripts/game.gd
+++ scripts/game.gd
@@ -17,10 +17,10 @@ var CHARACTERS = {
 
 #TEMP
 @onready var button = $Button
-var current_text_speed = 20
-var current_speak_speed = 0.03
+var current_speak_speed = 50
+var current_text_speed = 0.015
 
-var dialogue_test = "lorem isputim weird let's go hahah, let's goooooo"
+var dialogue_test = "Hello mama mia bekka de boopa."
 
 func _ready():
 	load_characters()
@@ -30,5 +30,7 @@ func load_characters():
 	CHARACTERS.YAGOO.sprite = conversation_controller.character_controller.yagoo
 
 func _on_button_pressed():
-	conversation_controller.display_dialogue(CHARACTERS.ACHAN, dialogue_test, "happy", current_text_speed, current_speak_speed)
+	conversation_controller.queue_dialogue(CHARACTERS.ACHAN, dialogue_test, "happy", current_speak_speed, current_text_speed)
+	conversation_controller.queue_dialogue(CHARACTERS.YAGOO, "alkdjflkfjladksjafslkjfls", "happy", current_speak_speed, current_text_speed)
+	conversation_controller.read_from_queue()
 	pass # Replace with function body.
