df803cf1559e5aa2cd7f229e2f06d7391d4504c7
misnina <misnina.k@gmail.com>
2024-06-20 16:45:13 -0500

    better relationship to score

--- scripts/big_blast.gd
+++ scripts/big_blast.gd
@@ -7,10 +7,10 @@ extends Node2D
 signal pump
 signal finished_pump
 
-@export var base_addition = 5
-@export var top_percent = 15
+@export var base_addition = 3
+@export var top_percent = 20
 @export var indicator_percent = 30
-@export var bottom_percent = 50
+@export var bottom_percent = 30
 @export var bonus_amount = 3.5
 @export var pump_timing = 1.2
 
@@ -40,23 +40,24 @@ func _process(delta):
 
 func _on_pump(time):
 	var time_left = pump_timing - time
+	var score_amount = base_addition - (time_left * 3)
 	var time_left_percent = (time_left / pump_timing) * 100
 	if (time_left_percent <= top_percent):
 		print('grabbed bonus')
 		print(time)
-		progress_bar.value += base_addition * bonus_amount
+		progress_bar.value += score_amount * bonus_amount
 	elif (time_left_percent >= bottom_percent):
 		print('too fast')
 		print(time)
-		progress_bar.value += base_addition / (bonus_amount)
+		progress_bar.value += score_amount / (0.5)
 	else:
 		print(time)
-		progress_bar.value += base_addition
+		progress_bar.value += score_amount * base_addition
 
 
 func stobe_indicator(time):
 	var progression_percentage = ((time / pump_timing) * 100)
-	if (progression_percentage > 80 and has_first_pump and not progression_percentage > 95):
+	if (progression_percentage > 80 and has_first_pump and not progression_percentage > 100):
 		if (color_switch):
 			await get_tree().create_timer(0.05).timeout
 			flash_indicator.modulate = Color(0, 0, 0)
